899523071fe15d76777b37481d7d398872d6e0ce
Cristiano Ramos <limaramos.c@gmail.com>
2020-11-12 12:27:43 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/cristianoramos/mc/domain/Categoria.java
@@ -0,0 +1,63 @@
+package com.cristianoramos.mc.domain;
+
+import java.io.Serializable;
+
+//objetos dela poderão ser convertidos em uma sequência de bytes, para q os objetos possam ser gravados em arquivos, trafegar em rede..
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	//Instancia objetos sem jogar nada para os atributos.
+	public Categoria() {
+		
+	}
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//Dois objetos são comparados pelo seu conteúdo e não pelo ponteiro de memória e compara obetos por valor.
+	//normalmente usa ID para comparar, se o ID é diferente, os objetos também são
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
